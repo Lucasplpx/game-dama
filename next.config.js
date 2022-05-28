105201d3ceaf8be6ca4edb74bab3c5c767c2356d
Lucas Passos <lucasplpx@gmail.com>
2022-05-28 11:54:17 -0300

    build ignore types

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
